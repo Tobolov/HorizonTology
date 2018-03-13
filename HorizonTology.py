455efd31c1fbb601259db04e30ea7f8ba1f631d1
Matthew Tobolov <matthew.tobolov@outlook.com>
2018-03-13 23:01:31 +1000

    First use of Flask
    We are now using PIL to create text matrices

--- HorizonTology.py
+++ HorizonTology.py
@@ -1,11 +1,36 @@
 from flask import Flask
+from PIL import Image, ImageFont, ImageDraw
+
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return text_to_dot_map("Python PIL")
+
+
+def text_to_dot_map(textToDisplay):
+    font = ImageFont.truetype('arialbd.ttf', 15)
+    size = font.getsize(textToDisplay)  # calc the size of text in pixels
+    image = Image.new('1', size, 1)  # create a b/w image
+    draw = ImageDraw.Draw(image)
+    draw.text((0, 0), textToDisplay, font=font)  # render the text to the bitmap
+
+    stringoutput = ""
+    for row in range(size[1]):
+        for column in range(size[0]):
+            stringoutput += map_bit_to_char(image, column, row)
+
+        stringoutput += "<br>"
+
+    return stringoutput
+
+
+def map_bit_to_char(im, col, row):
+    if im.getpixel((col, row)):
+        return '&nbsp;&nbsp;'
+    else:
+        return '#'
 
 
 if __name__ == '__main__':
